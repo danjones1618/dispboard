09a23f19d635eb18b931c4786e3caff4589afe29
danjones1618 <hello@danjones.dev>
2020-04-10 14:21:28 +0100

    Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+//import background from './background.png';
 import './App.css';
 
-function App() {
+function Slide(props){
   return (
-    <div className="App">
-
-    </div>
+    <div className="slide" style={{backgroundImage: `url(${props.bg})`}}>{props.children}</div>
   );
 }
 
+function TimeSlide(props){
+  var endTime = new Date("2020-03-01 11:00");
+  var startDay = new Date("2020-02-29");
+  var delta = new Date(endTime - (new Date()) + startDay);
+  return (
+    <Slide className="timeslide" bg={props.bg}>
+      <h1>{delta.toTimeString().split(" ")[0]}</h1>
+      <h3>Have you signed up your team?</h3>
+    </Slide>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <TimeSlide bg="none"/>
+    </>
+  );
+}
 export default App;
