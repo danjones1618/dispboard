b57398e5b01e5e4e3b324ef1cee4d7940ea155a6
danjones1618 <hello@danjones.dev>
2020-06-07 22:41:50 +0100

    Set 16/9 slide size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 //import background from './background.png';
 import './App.css';
 
 function Slide(props){
   return (
-    <div className="slide" style={{backgroundImage: `url(${props.bg})`}}>{props.children}</div>
+    <div className={`slide ${props.className}`} style={props.bg !== undefined ? {backgroundImage: `url(${props.bg})`} : undefined}>
+      {props.children}
+    </div>
   );
 }
 
-function TimeSlide(props){
-  var endTime = new Date("2020-03-01 11:00");
-  var startDay = new Date("2020-02-29");
-  var delta = new Date(endTime - (new Date()) + startDay);
+function CenterSlide(props) {
   return (
-    <Slide className="timeslide" bg={props.bg}>
-      <h1>{delta.toTimeString().split(" ")[0]}</h1>
+    <Slide {...props} className={`centerslide ${props.className}`}/>
+  )
+}
+
+function TimeSlide(props){
+  return (
+    <CenterSlide className="timeslide" bg={props.bg}>
+      <h1>timeLeft</h1>
       <h3>Have you signed up your team?</h3>
-    </Slide>
+    </CenterSlide>
   );
 }
 
 function App() {
   return (
     <>
-      <TimeSlide bg="none"/>
+      <TimeSlide/>
     </>
   );
 }
